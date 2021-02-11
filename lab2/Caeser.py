892860bc8bb6e7a353405cb13b3c8f17ea0da3fc
Long <ln24630@gmail.com>
2021-02-10 23:30:49 -0800

    tested and fixed the else clause of the encrypt function

--- lab2/Caeser.py
+++ lab2/Caeser.py
@@ -9,14 +9,20 @@ def printMenu(): pass
 
 def encrypt(userInput, key):
     cipher = ""
-    for i in range(userInput):
+    for i in range(len(userInput)):
         result = userInput[i].isalpha()
+
+        #If it is a letter then shift the index therefore encrypt it
         if result:
-            indexPos = alphaList.index(userInput[i])
-            enIndex  = (indexPos + key) % 26
+            userUpper = userInput[i].upper()
+            #indexPos = alphaList.index(userInput[i].upper())
+            indexPos = alphaList.index(userUpper)
+            enIndex = (indexPos + key) % 26
             cipher = cipher + alphaList[enIndex]
+
+        #not a letter just add to the cipher text
         else:
-            cipher = userInput[i]
+            cipher = cipher + userInput[i]
 
     return cipher
 
@@ -24,8 +30,10 @@ def encrypt(userInput, key):
 
 def main():
     user_input = takeInput()
+    cipher = encrypt(user_input, 2)
     print("Size of user string: " + str(len(user_input)))
     print("Check for letters: " + str(user_input.isalpha()))
+    print("Cipher text:" + cipher)
     print("End of program")
 
 main()
