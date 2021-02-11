e48c400dac40772a787cec360d63675bf4f13845
Neo <ln24630@gmail.com>
2021-02-10 22:31:55 -0800

    create lab 2 folder

---
+++ lab2/Caeser.py
@@ -0,0 +1,16 @@
+alphabetList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J","K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
+                "V", "W", "X", "Y", "Z"]
+
+def takeInput():
+    user_input = input("Enter something to encrypt or decrypt: ")
+
+def printMenu(): pass
+
+def encrypt(): pass
+
+
+def main():
+    takeInput()
+    print("End of program")
+
+main()
