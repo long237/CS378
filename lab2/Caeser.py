dd37f0c0a4b3cecd089b42552abbff1e6292a865
Neo <ln24630@gmail.com>
2021-02-11 15:16:51 -0800

    finish the main with asking for input

--- lab2/Caeser.py
+++ lab2/Caeser.py
@@ -9,14 +9,23 @@ def getInt():
     user_input = input("Enter a key: ")
     while not user_input.isnumeric():
         user_input = input("Enter a key: ")
+    return int(user_input)
+
+def getOp():
+    user_input = input("Enter an option: ")
+    while (not user_input.isnumeric()) or (int(user_input) < 1) or (int(user_input) > 3):
+        user_input = input("Enter an option: ")
     return user_input
 
-def printMenu(): pass
+def printMenu():
+    print("1. Encrypt \n"
+          "2. Decrypt \n"
+          "3. Exit")
 
 def convert(userInput, key, flag):
     reValue = ""
 
-    if flag == 0:
+    if flag == 1:
         for i in range(len(userInput)):
             result = userInput[i].isalpha()
 
@@ -32,7 +41,7 @@ def convert(userInput, key, flag):
                 reValue = reValue + userInput[i]
 
     #Decryption
-    elif flag == 1:
+    elif flag == 2:
         for i in range(len(userInput)):
             result = userInput[i].isalpha()
 
@@ -52,11 +61,23 @@ def convert(userInput, key, flag):
 
 
 def main():
-    user_input = takeInput()
-    userInt = getInt()
-    cipher = convert(user_input, 2, 1)
-    print("Size of user string: " + str(len(user_input)))
-    print("Cipher text:" + cipher)
-    print("End of program")
+    # user_input = takeInput()
+    # userInt = getInt()
+    # cipher = convert(user_input, 19, 1)
+    # print("Size of user string: " + str(len(user_input)))
+    # print("Cipher text:" + cipher)
+    # print("End of program")
+
+    user_op = -1
+    while user_op != 3:
+        printMenu()
+        user_op = int(getOp())
+        if user_op != 3:
+            user_str = takeInput()
+            user_key = getInt()
+            out = convert(user_str, user_key, user_op)
+            print("Output: " + out)
+        else:
+            print("Good bye")
 
 main()
