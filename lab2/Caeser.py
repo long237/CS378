2266609648d5b71d945a82ec2e284b083efbef41
Neo <ln24630@gmail.com>
2021-02-14 20:32:33 -0800

    create a function to calculate index to shorten the code

--- lab2/Caeser.py
+++ lab2/Caeser.py
@@ -22,55 +22,39 @@ def printMenu():
           "2. Decrypt \n"
           "3. Exit")
 
-def convert(userInput, key, flag):
+def indexCal(index, key, flag):
+    newIndex = -1
+    if (flag == 1):
+        newIndex = (index + key) % 26
+    elif flag == 2:
+        newIndex = (index + (26 - key)) % 26
+
+    return newIndex
+
+def en_deCode(userInput, key, flag):
     reValue = ""
 
-    if flag == 1:
-        for i in range(len(userInput)):
-            result = userInput[i].isalpha()
+    for i in range(len(userInput)):
+        result = userInput[i].isalpha()
 
-            #If it is a letter then shift the index therefore encrypt it
-            if result:
-                lowerBool = userInput[i].islower()
-                userUpper = userInput[i].upper()
-                indexPos = alphaList.index(userUpper)
-                enIndex = (indexPos + key) % 26
+        # If it is a letter then shift the index therefore encrypt it
+        if result:
+            lowerBool = userInput[i].islower()
+            userUpper = userInput[i].upper()
+            indexPos = alphaList.index(userUpper)
+            newIndex = indexCal(indexPos,key,flag)
 
-                #if the letter was lower case, make it lower case again
-                ciLetter = alphaList[enIndex]
-                if lowerBool:
-                    ciLetter = alphaList[enIndex].lower()
-                reValue = reValue + ciLetter
+            # if the letter was lower case, make it lower case again
+            ciLetter = alphaList[newIndex]
+            if lowerBool:
+                ciLetter = alphaList[newIndex].lower()
+            reValue = reValue + ciLetter
 
-            #not a letter just add to the cipher text
-            else:
-                reValue = reValue + userInput[i]
-
-
-    #Decryption
-    elif flag == 2:
-        for i in range(len(userInput)):
-            result = userInput[i].isalpha()
-
-            # If it is a letter then shift the index therefore encrypt it
-            if result:
-                lowerBool = userInput[i].islower()
-                userUpper = userInput[i].upper()
-                indexPos = alphaList.index(userUpper)
-                deIndex = (indexPos + (26 - key)) % 26
-
-                ciLetter = alphaList[deIndex]
-                if lowerBool:
-                    ciLetter = alphaList[deIndex].lower()
-                reValue = reValue + ciLetter
-
-            # not a letter just add to the cipher text
-            else:
-                reValue = reValue + userInput[i]
+        # not a letter just add to the cipher text
+        else:
+            reValue = reValue + userInput[i]
 
     return reValue
-
-
 
 def main():
     # user_input = takeInput()
@@ -87,7 +71,8 @@ def main():
         if user_op != 3:
             user_str = takeInput()
             user_key = getInt()
-            out = convert(user_str, user_key, user_op)
+            #out = convert(user_str, user_key, user_op)
+            out = en_deCode(user_str, user_key, user_op)
             print("Output: " + out)
         else:
             print("Good bye")
